00517b7a79b02c0543fb6c45dd6d1f17d2db1b71
lutsjaroslav <jaroslav.luts@gmail.com>
2018-04-25 17:28:58 +0300

    load file update setuser

---
+++ set_user.php
@@ -0,0 +1,21 @@
+<?php include_once 'template/header.php';?>
+
+<?php include_once 'template/nav_menu.php';?>
+
+<div class="settings_page">
+	<div class="row">
+		<div class="col-12 intro_text">
+			<h3 class="set_title">Add Username</h3>
+			<p>Will be displayed in the tests and greetings. If not specified, the login (unique) will be used by default</p>
+		</div>
+        <div class="col-12">
+        	<div class="col-6">
+				<div class="form-group">
+		          <input type="text" class="form-control" id="inputLogin3" placeholder="Login">
+		        </div>
+		    </div>
+      </div>
+	</div>
+</div>
+
+<?php include_once 'template/footer.php';?>
